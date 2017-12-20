47acba63fa161dc51de81590f52c54126afbb730
Julia Chotoo <julia.chotoo@gmail.com>
2017-12-20 08:45:45 -0500

    [Finished article scraper ACS and Springer]

--- articles.py
+++ articles.py
@@ -36,32 +36,6 @@ class ArticleItem(Item):
     abstract = Field()
     text = Field()
     figures = Field()
-
-#Pipeline to turn data into JSON file for ACS
-#class JsonWriterPipeline(object):
-#    def open_spider(self, spider):
-#        self.file = open('result.jl', 'w')
-        
-#    def close_spider(self, spider):
-#        self.file.close()
-
-#    def process_item(self, item, spider):
-#        line = json.dumps(dict(item)) + "\n"
-#        self.file.write(line)
-#        return item
-
-#Pipeline for Springer
-#class JsonWriterPipelineSpr(object):
-#    def open_spider(self, spider):
-#        self.file = open(namespr, 'w')
-
-#    def close_spider(self, spider):
-#        self.file.close()
-
-#    def process_item(self, item, spider):
-#        line = json.dumps(dict(item)) + "\n"
-#        self.file.write(line)
-#        return item
  
 #Defining variable for spiders and pipeline
 full_url_acs = ''
@@ -94,8 +68,6 @@ for d in doi_lst:
         
         doiacs_name = re.sub('/', '', doi_acs)
         nameacs = 'doiacs-%s.json' %(doiacs_name)
-
-        print(nameacs)
         
     elif bib_info['publisher'] == 'Springer Nature':
         
@@ -117,10 +89,6 @@ class ArticleSpider(scrapy.Spider):
     name = 'ArticleSpider'
     allowed_domains = ["http://pubs.acs.org/"]
     start_urls = full_url_acs_lst
-    
- #   custom_settings = {
- #       'ITEM_PIPELINES': {'__main__.JsonWriterPipeline': 2,}
- #   }
             
     def parse(self, response):
         item = ArticleItem()
@@ -136,17 +104,13 @@ class ArticleSpiderSpr(scrapy.Spider):
     name = 'ArticleSpiderSpr'
     allowed_domains = ["https://link.springer.com"]
     start_urls = full_url_spr_lst
-            
-#    custom_settings = {
-#        'ITEM_PIPELINES': {'__main__.JsonWriterPipelineSpr': 1,}
-#    }
 
     def parse(self, response):
         item = ArticleItem()
         item['title'] = response.xpath('//h1[@class="ArticleTitle"]/text()').extract()
         item['authors'] = response.xpath('//span[@class="authors__name"]/text()').extract()
         item['doi'] = response.xpath('//span[@id="doi-url"]/text()').extract()
-#        item['abstract'] = response.xpath('//p[@class="SimplePara"]/text()').extract()
+        item['abstract'] = []
         item['text'] = response.xpath('//div[@id="body"]/descendant::text()').extract()
         item['figures'] = response.xpath('//div[@class="MediaObject"]').extract()
         yield item
@@ -154,12 +118,14 @@ class ArticleSpiderSpr(scrapy.Spider):
 def run():
 
     settings = get_project_settings()
-    settings.set('FEED_FORMAT', 'json')
-    settings.set('FEED_URI', 'result5.jl')
+    settings.set('FEED_FORMAT', 'jsonlines')
+    settings.set('FEED_URI', 'result6.jl')
 
     configure_logging()
     runner = CrawlerRunner(settings)
-    runner.crawl(ArticleSpiderSpr, ArticleSpider)
+
+    runner.crawl(ArticleSpider)
+    runner.crawl(ArticleSpiderSpr)
 
     d = runner.join()
     d.addBoth(lambda _: reactor.stop())
@@ -168,42 +134,4 @@ def run():
 
 if __name__ == '__main__':
     run()
-    
-#Runs both Spiders
-#process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
-#process.crawl(ArticleSpider)
-#process.crawl(ArticleSpiderSpr)
-#process.start()
-
-##Opens data scraped into many lists, edits file to concatenate lists
-#def concatenate_list(input):
-#    output = ''
-#    for item in input:
-#        output = output+item
-#        return output
-
-#for d in doi_list:
-    
-#    resultACS = json.load(open(nameacs,mode='r'))
-#    resultACS['abstract'] = concatenate_list(resultACS['abstract'])
-#    resultACS['text'] = concatenate_list(resultACS['text'])
-
-#    with open(nameacs, 'w') as jsonFile:
-#        json.dump(resultACS, jsonFile)
-
-
-#    resultSpr = json.load(open(namespr, mode='r'))
-#    resultSpr['text'] = concatenate_list(resultSpr['text'])
-
-#    fixed_authors = []
-#    for x in resultSpr['authors']:
-#        fixed_authors.append(re.sub('\xa0', ' ', x))
-#    resultSpr['authors'] = fixed_authors
-#    with open(namespr, 'w') as jsonFile:
-#        json.dump(resultSpr, jsonFile)
-
-
-
-#Problem going back in to concatenate texts and remove tags
-#more than one file opening at a time doesn't work
 
